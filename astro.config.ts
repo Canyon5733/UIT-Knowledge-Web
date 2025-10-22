2d1d9a69e365eb59d5a9d0777028973729c3469e
Noah Trần <trananhquan1009@gmail.com>
2025-10-22 20:12:41 +0700

    :sparkles: - added svelte

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
 
 export default defineConfig({
   site: 'https://uitknowledge.org',
+
+  integrations: [svelte()],
 
   server: {
     port: 3000,
