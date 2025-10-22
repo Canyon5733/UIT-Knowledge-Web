63edb77e3a6db8952fc22289e035dd05a5ab2c9d
Noah Trần <trananhquan1009@gmail.com>
2025-10-22 19:36:52 +0700

    :sparkles: - setup lint/format/pre-commit

---
+++ .prettierrc.mjs
@@ -0,0 +1,27 @@
+// Template from github.com/rice-cracker-dev/rice-dev.tech
+
+/** @type {import("prettier").Config} */
+
+export default {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  htmlWhitespaceSensitivity: 'css',
+  trailingComma: 'es5',
+  plugins: [
+    'prettier-plugin-astro',
+    'prettier-plugin-tailwindcss',
+    'prettier-plugin-svelte',
+  ],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+
+    { files: '*.svelte', options: { parser: 'svelte' } },
+  ],
+};
